a5364c544def9f347eb97dcfc6085b0d69e84402
Aayam Adhikari <thisisleo7@gmail.com>
2023-09-29 16:01:32 +0545

    Initial Commit

---
+++ SahaYatri/SahaYatriApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahaYatriApp.swift
+//  SahaYatri
+//
+//  Created by Aayam Adhikari on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SahaYatriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
